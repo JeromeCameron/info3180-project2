6791eb759726fd02a7f58e2ba845215058a8c0c0
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2019-04-26 18:09:10 +0000

    Update DB URI

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = "@#$%my$ecretKey@#$%*"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:project2@localhost/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "xllccityhagvdw:44782820358f59ce9d1ec19fa218328a19aba69ffcb24cfb2b1b8e049dd0a0bc@ec2-54-235-114-242.compute-1.amazonaws.com:5432/d1stvv7mrp01ga" #"postgresql://project2:project2@localhost/project2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
